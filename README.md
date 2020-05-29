a7b32df4311893883d499185b1123c3c0d49e55f
monmima <monmima@protonmail.com>
2020-05-28 22:56:59 -0400

    working on joi validation, buggy for now

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 
 MongoDB Tutorial - CRUD app from scratch using Node.js
 
-Many thanks to freeCodeCamp.org and NoobCoder for creating this tutorial
+Many thanks to freeCodeCamp.org and NoobCoder/Pedro Mercado for creating this tutorial
 
-https://www.youtube.com/watch?v=CyTWPr_WwdI
-https://github.com/noobcoder1137/Todo_Rest_CRUD_Application_JQuery_FetchAPI
+- https://www.youtube.com/watch?v=CyTWPr_WwdI
+- https://github.com/noobcoder1137/Todo_Rest_CRUD_Application_JQuery_FetchAPI
 
-This branch contains validation with Joi
+## Joi
+
+This branch contains validation with Joi (which at 59:00 in Noobcoder's video).
+
+21:45
--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
-app.use(bodyParser.json());
 const path = require("path");
+const Joi = require("joi");
 
 const db = require("./db");
 const collection = "todo";
+const app = express();
+
+const schema = Joi.object().keys({
+    todo: Joi.string.required()
+});
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
@@ -39,20 +45,29 @@ app.put("/:id", (req, res) => {
 });
 
 // create
-app.post("/", (req, res) => {
+app.post("/", (req, res, next) => {
     console.log("in post");
     const userInput = req.body;
 
-    // connect to the database
-    db.getDB().collection(collection).insertOne(userInput, (err, result) => {
-        // in production, you'll want to do more, like displaying an error message to the user
+    Joi.validate(userInput, schema, (err, result) => {
         if (err) {
-            console.log(err);
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
         } else {
-            res.json({result: result, document: result.ops[0]});
+            // connect to the database
+            db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+                // in production, you'll want to do more, like displaying an error message to the user
+                if (err) {
+                    const error = new Error("Failed to insert Todo Document");
+                    error.status = 400;
+                    next(error);
+                } else {
+                    res.json({result: result, document: result.ops[0], msg: "Successfully inserted Todo!", error: null});
+                }
+            });
         }
     });
-
 });
 
 // delete
@@ -68,6 +83,15 @@ app.delete("/:id", (req, res) => {
             res.json(result);
         }
     });
+});
+
+// middleware
+app.use((err, req, res, next) => {
+    res.status(err.status).json({
+        error: {
+            message: err.message
+        }
+    })
 });
 
 // connect to the database
